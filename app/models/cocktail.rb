af2344d81cbe1ad00f092f154f73d01a9cff2e8e
ECUK Pairing Machine <ecuk-pairing@edgecase.com>
2011-07-04 17:14:40 +0100

    remove measured ingredients for a cocktail when deleted.

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,7 @@
 class Cocktail < ActiveRecord::Base
-  has_many :measured_ingredients
+  has_many :measured_ingredients, :dependent => :destroy
+
+  accepts_nested_attributes_for :measured_ingredients
 
   validates :name, :presence   => true,
                    :uniqueness => true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,11 @@
-# Initial ingredients
-["Grenadine", "Soda Water", "Campari", "Bols Genever", "Vermouth", "Maraschino Cherry", "Lemon Juice"].each do |ingredient_name|
-  Ingredient.find_or_create_by_name(:name => ingredient_name)
-end
+# Watermelon & Vodka Cooler
+watermelon = Ingredient.create!(:name => 'Watermelon')
+vodka      = Ingredient.create!(:name => 'Vodka')
+triple_sec = Ingredient.create!(:name => 'Triple Sec')
+lime_juice = Ingredient.create!(:name => 'Lime Juice')
+
+cocktail = Cocktail.create!(:name => 'Watermelon & Vodka Cooler')
+cocktail.measured_ingredients.create!(:ingredient => watermelon, :measure => '4 measures')
+cocktail.measured_ingredients.create!(:ingredient => vodka,      :measure => '1 measure')
+cocktail.measured_ingredients.create!(:ingredient => triple_sec, :measure => '1/2 measure')
+cocktail.measured_ingredients.create!(:ingredient => lime_juice, :measure => '1/4 measure')
