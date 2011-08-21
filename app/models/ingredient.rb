6daaf0727f6fa313fa0162ae13ede5766f14a3d4
Mark Connell <mark.connell@edgecase.com>
2011-08-21 23:44:05 +0100

    list cocktails for an ingredient

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,6 @@
 class Ingredient < ActiveRecord::Base
+  has_many :measured_ingredients
+  has_many :cocktails, :through => :measured_ingredients
 
   validates :name, :presence   => true,
                    :uniqueness => { :case_sensitive => false }
