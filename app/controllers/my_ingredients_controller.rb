76415e0eef1f08b2d3c5e0ce912f7c1d8088fc71
Mark Connell <mark.connell@edgecase.com>
2011-08-27 11:13:27 +0100

    whooopsy, add ingredients to my list

--- app/controllers/my_ingredients_controller.rb
+++ app/controllers/my_ingredients_controller.rb
@@ -1,13 +1,13 @@
 class MyIngredientsController < ApplicationController
 
   def update
-    ingredient = Ingredient.find(params[:id])
+    ingredient = Ingredient.find_by_url_slug(params[:id])
     my_ingredients << ingredient.id
     redirect_to :back
   end
 
   def destroy
-    ingredient = Ingredient.find(params[:id])
+    ingredient = Ingredient.find_by_url_slug(params[:id])
     remove_my_ingredient(ingredient)
     redirect_to :back
   end
