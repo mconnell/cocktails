56ca667a1818939507e5e6380aa49a09e8b642c7
Mark Connell <mark.connell@edgecase.com>
2011-08-27 11:16:57 +0100

    repair tests

--- spec/controllers/my_ingredients_controller_spec.rb
+++ spec/controllers/my_ingredients_controller_spec.rb
@@ -1,20 +1,20 @@
 require 'spec_helper'
 
 describe MyIngredientsController do
-  let(:ingredient){i=Ingredient.new; i.id = 123; i}
+  let(:ingredient){i=Ingredient.new; i.id = 123; i.url_slug = 'gin'; i}
   before(:each){ request.env["HTTP_REFERER"] = 'place_i_came_from' }
 
   describe "PUT #update" do
     it "adds the ingredient id to the session" do
-      Ingredient.stub(:find).and_return(ingredient)
-      put :update, :id => 123
+      Ingredient.stub(:find_by_url_slug).and_return(ingredient)
+      put :update, :id => 'gin'
       assert_equal [123], session[:my_ingredients]
     end
   end
 
   describe "DELETE #destroy" do
     it "removes the ingredient id from the session" do
-      Ingredient.stub(:find).and_return(ingredient)
+      Ingredient.stub(:find_by_url_slug).and_return(ingredient)
       session[:my_ingredients] = [123]
       delete :destroy, :id => 123
       assert_equal [], session[:my_ingredients]
